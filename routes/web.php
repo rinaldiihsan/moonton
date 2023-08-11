d7d1da5c6401b02aa255df7f17832d8787d7dfa3
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2023-08-11 20:20:17 +0700

    feat: Slicing Subscription

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
     route::get('/dashboard', function () {
         return Inertia::render('Prototype/Dashboard');
     })->name('dashboard');
+
+    route::get('/subscriptionPlan', function () {
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
 });
 
 require __DIR__ . '/auth.php';
